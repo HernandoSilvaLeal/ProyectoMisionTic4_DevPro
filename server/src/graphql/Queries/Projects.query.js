1954ee9326a4183212147e4c6983a92591c37d87
Walther Seidel <waseidel@gmail.com>
2021-11-04 11:36:57 -0500

    Correcciones en el arcivo projets.query.js
    - se agregó el import { ROLE_ENUM } = require("../Enums/Role.enum");

--- server/src/graphql/Queries/Projects.query.js
+++ server/src/graphql/Queries/Projects.query.js
@@ -4,14 +4,17 @@ const { isTokenValid } = require("../../helper/auth");
 const Project = require("../models/project.model");
 const ProjectType = require("../types/project_type");
 
+const { ROLE_ENUM } = require("../Enums/Role.enum");
+
 const projects = {
   type: new GraphQLList(ProjectType),
   async resolve(_, __, context) {
     const user = await isTokenValid(context.token);
 
-    if (user.role === ROLE_ENUM.getValue("STUDENT").value) {
-      return Project.find({});
+    if (user.role === ROLE_ENUM.getValue("LEADER").value) {
+      return Project.find({ leader: user.id });
     }
+
     return Project.find({});
   },
 };
